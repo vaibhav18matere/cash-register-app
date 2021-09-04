e0b71e6ddbe5807d3d765ae160854da34f9e3260
VaibhavMatere <59862355+vaibhav18matere@users.noreply.github.com>
2021-09-04 18:02:25 +0530

    image added

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ function calculateChange(amountToBeReturned) {
     for (let i = 0; i < availableNotes.length; i++) {
         const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
 
-        amountToBeReturned %= availableNotes[i];
+        amountToBeReturned = amountToBeReturned % availableNotes[i];
         noOfNotes[i].innerText = numberOfNotes;
     }
 }

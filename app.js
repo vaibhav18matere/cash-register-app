17042b0194ca88cfa8a6a436bb8aff91da6640d9
VaibhavMatere <59862355+vaibhav18matere@users.noreply.github.com>
2021-09-22 23:52:57 +0530

    style : display hide and show

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-console.log("it's working");
-
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
+
 const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
 
-const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+const cashGivenDiv = document.querySelector(".cash-given-div");
+const resultSectionDiv = document.querySelector(".result_section");
 
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
     hideMessage();
@@ -15,6 +16,7 @@ checkButton.addEventListener("click", function validateBillAndCashAmount() {
         if (cashGiven.value >= billAmount.value) {
             const amountToBeReturned = cashGiven.value - billAmount.value;
             calculateChange(amountToBeReturned);
+            resultSectionDiv.style.display = "flex";
         } else {
             showMessage(
                 "Cash provided should be equal to the bill amount")
@@ -23,11 +25,13 @@ checkButton.addEventListener("click", function validateBillAndCashAmount() {
         showMessage("Invalid bill amount");
     }
 });
+
 function calculateChange(amountToBeReturned) {
     for (let i = 0; i < availableNotes.length; i++) {
         const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
 
         amountToBeReturned = amountToBeReturned % availableNotes[i];
+        console.log(numberOfNotes)
         noOfNotes[i].innerText = numberOfNotes;
     }
 }
@@ -40,4 +44,11 @@ function showMessage(msg) {
     message.style.display = "block";
     message.innerText = msg;
 }
+
+function showNextContainer() {
+    cashGivenDiv.style.display = "block";
+}
+
+billAmount.addEventListener("change", showNextContainer)
+
 

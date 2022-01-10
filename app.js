490606e02903ed18a6f7e4a82a15e666134d96bf
VaibhavMatere <59862355+vaibhav18matere@users.noreply.github.com>
2022-01-10 13:57:18 +0530

    refractor : convert code to es6 standards

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
-
 const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
-
 const cashGivenDiv = document.querySelector(".cash-given-div");
 const resultSectionDiv = document.querySelector(".result_section");
-
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
-checkButton.addEventListener("click", function validateBillAndCashAmount() {
+checkButton.addEventListener("click", validateBillAndCashAmount = () => {
     hideMessage();
+
     if (billAmount.value > 0) {
         if (cashGiven.value >= billAmount.value) {
             const amountToBeReturned = cashGiven.value - billAmount.value;
@@ -26,26 +24,25 @@ checkButton.addEventListener("click", function validateBillAndCashAmount() {
     }
 });
 
-function calculateChange(amountToBeReturned) {
+const calculateChange = (amountToBeReturned) => {
     for (let i = 0; i < availableNotes.length; i++) {
         const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
 
         amountToBeReturned = amountToBeReturned % availableNotes[i];
-        console.log(numberOfNotes)
         noOfNotes[i].innerText = numberOfNotes;
     }
 }
 
-function hideMessage() {
+const hideMessage = () => {
     message.style.display = "none";
 }
 
-function showMessage(msg) {
+const showMessage = (msg) => {
     message.style.display = "block";
     message.innerText = msg;
 }
 
-function showNextContainer() {
+const showNextContainer = () => {
     cashGivenDiv.style.display = "block";
 }
 
